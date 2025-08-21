0464861386adda3e7e4056be4f4f0478fdb22767
tunashred <enachealexandru.traian@gmail.com>
2025-08-21 19:57:45 +0300

    Add more constants to utils

--- tests/utils/include/utils/utils.hpp
+++ tests/utils/include/utils/utils.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <array>
 #include <cstdlib>
 
+// TODO: move all the constants to a separate file
 constexpr size_t CACHE_LINE = 64;
 
 constexpr size_t ALIGNMENT_32      = 32;
@@ -10,8 +12,11 @@ constexpr size_t SIMD_LONG_WIDTH   = 4;
 constexpr size_t SIMD_DOUBLE_WIDTH = 4;
 
 constexpr size_t LOOP_COUNT_18   = 18;
-constexpr size_t LOOP_COUNT_200K = 200000;
-constexpr size_t LOOP_COUNT_400K = 400000;
+constexpr size_t LOOP_COUNT_200K = 200'000;
+constexpr size_t LOOP_COUNT_400K = 400'000;
+constexpr size_t LOOP_COUNT_1M   = 1'000'000;
+
+constexpr std::array<size_t, 3> small_pow2 = {8, 16, 32};
 
 void* safe_malloc(size_t size);
 
