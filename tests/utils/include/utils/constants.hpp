c8b37b4148f03be4b79e90c9b8dde1afa976d939
tunashred <enachealexandru.traian@gmail.com>
2025-09-30 23:40:09 +0300

    Separated components into libs which compile with different flags

--- tests/utils/include/utils/constants.hpp
+++ tests/utils/include/utils/constants.hpp
@@ -3,7 +3,10 @@
 #include <array>
 #include <cstddef>
 #include <tuple>
+
+#ifdef __AVX__
 #include <immintrin.h>
+#endif
 
 constexpr size_t CACHE_LINE = 64;
 
@@ -44,6 +47,7 @@ constexpr std::array<std::tuple<double, double, double>, 5> mandelbrot_args = {{
 
 constexpr int ITER_1500 = 1500;
 
+#ifdef __AVX__
 const __m256d simd_doubles_2 = _mm256_set1_pd(2);
 
 const __m256d simd_doubles_minus_2 = _mm256_set1_pd(-2);
@@ -53,3 +57,4 @@ const __m128i simd_ints_1500 = _mm_set1_epi32(1500);
 const __m256d simd_doubles_true = _mm256_castsi256_pd(_mm256_set1_epi64x(-1));
 
 const __m128i simd_ints_1 = _mm_set1_epi32(1);
+#endif
